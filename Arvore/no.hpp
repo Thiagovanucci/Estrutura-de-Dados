18ba79e10efd63295950273b74a45644f51f7a1b
Thiago Vanucci <thiagovanucci20@gmail.com>
2022-11-05 00:02:46 -0300

    abp1

--- Arvore/no.hpp
+++ Arvore/no.hpp
@@ -1,12 +1,14 @@
 
-class No{
-    private:
+typedef class No{
+    public:
         int dado;
         No* esq;
         No* dir;
-    public:
-        No();
-        int cria();
-        int vazia();
-        No busca();
-};
+}arvore;
+
+typedef arvore *arvoreBinaria;
+
+void cria();
+void vazia(No* r);
+arvoreBinaria busca(arvoreBinaria r,int conteudo);
+int exibirArvore(arvoreBinaria r);
